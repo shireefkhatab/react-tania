f76f72387d44f5bf02ab3f7f9f7fca534fbbaba4
Reef <sherifkhattab13@gmail.com>
2018-10-23 21:09:34 +0100

    1st commit

---
+++ precache-manifest.6fb3bb669e1a5031304992265d3d9f77.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "2a8348a1a452d81a04bd",
+    "url": "/react-tania/static/js/runtime~main.2a8348a1.js"
+  },
+  {
+    "revision": "511a047b0ef2adb1c1fc",
+    "url": "/react-tania/static/js/main.511a047b.chunk.js"
+  },
+  {
+    "revision": "7eae6140ad0adf38fbdc",
+    "url": "/react-tania/static/js/1.7eae6140.chunk.js"
+  },
+  {
+    "revision": "511a047b0ef2adb1c1fc",
+    "url": "/react-tania/static/css/main.e13be2e9.chunk.css"
+  },
+  {
+    "revision": "e17701862c9d15eef33341ec07f6d27b",
+    "url": "/react-tania/index.html"
+  }
+];
